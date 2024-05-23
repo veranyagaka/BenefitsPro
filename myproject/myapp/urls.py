5a32609cd9ebc8b32f5ce3192f7b9bfa6c26c6cf
veranyagaka <nyagakaver@gmail.com>
2024-05-23 08:25:28 +0300

    inital reset password setup

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views 
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,5 +9,9 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile_view'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
-
+    #reset password
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete')
 ]
