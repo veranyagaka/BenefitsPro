d1a84894e70a3c58028808823bffb795f0ede907
veranyagaka <nyagakaver@gmail.com>
2024-05-25 23:53:18 +0300

    benefits model and migration

--- myproject/benefits/models.py
+++ myproject/benefits/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Benefits(models.Model):
+    name = models.CharField(max_length=255)
+    duration = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- myproject/enrollment/views.py
+++ myproject/enrollment/views.py
@@ -1,35 +1,32 @@
 from django.shortcuts import render, redirect
 from .models import EmployeeBenefits, Employee
+from benefits.models import Benefits
 
 def enrollment(request):
-    return render(request, 'enrollment.html')
+    #return render(request, 'enrollment.html')
+    benefits = Benefits.objects.all()
+    return render(request, 'enrollment.html', {'benefits': benefits})
 
 def process_enrollment(request):
     if request.method == 'POST':
         employee_id = request.POST['employee_id']
         last_name = request.POST['last_name']
         first_name = request.POST['first_name']
-        health_insurance = 'health_insurance' in request.POST
-        dental_insurance = 'dental_insurance' in request.POST
-        vision_insurance = 'vision_insurance' in request.POST
-        life_insurance = 'life_insurance' in request.POST
-        retirement_savings = 'retirement_savings' in request.POST
+        selected_benefits = request.POST.getlist('benefits[]')
 
+        
         # Check if employee exists in Employee table
         if Employee.objects.filter(employee_id=employee_id, last_name=last_name, first_name=first_name).exists():
-            # Create or update employee benefits
-            EmployeeBenefits.objects.update_or_create(
-                employee_id=employee_id,
-                defaults={
-                    'last_name': last_name,
-                    'first_name': first_name,
-                    'health_insurance': health_insurance,
-                    'dental_insurance': dental_insurance,
-                    'vision_insurance': vision_insurance,
-                    'life_insurance': life_insurance,
-                    'retirement_savings': retirement_savings
-                }
-            )
+            employee = Employee.objects.get(employee_id=employee_id, last_name=last_name, first_name=first_name)
+            
+            # Remove previous benefits
+            employee.employee_benefits.all().delete()
+            
+            # Add selected benefits to the employee
+            for benefit_id in selected_benefits:
+                benefit = Benefits.objects.get(id=benefit_id)
+                EmployeeBenefits.objects.create(employee=employee, benefit=benefit)
+            
             return redirect('profile')  # Redirect to profile page (make sure to define this URL)
         else:
             return render(request, 'enrollment_fail.html', {'message': 'Employee not found'})
