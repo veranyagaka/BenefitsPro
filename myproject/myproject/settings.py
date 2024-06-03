8efd83dccbc8313709e790cb96c789aa35c63995
veranyagaka <nyagakaver@gmail.com>
2024-06-03 18:57:03 +0300

    tt

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS=[BASE_DIR /'static']
+STATICFILES_DIRS=os.path.join(BASE_DIR, 'static/')
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #print(BASE_DIR)
 LOGIN_URL = 'login_view'
