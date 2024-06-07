6b74334b6c33f12f03aa35e7d7a8fc4308b2845c
veranyagaka <nyagakaver@gmail.com>
2024-06-07 20:01:27 +0300

    changepassword

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     #settings
     path('settings/', views.settings_view, name='settings'),
+    path('change-password/', views.change_password, name='change_password'),
 
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -132,5 +132,21 @@ def add_beneficiary(request):
     else:
         form = BeneficiaryForm()
     return render(request, 'add_beneficiary.html', {'form': form})
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
 
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {'form': form})
 
